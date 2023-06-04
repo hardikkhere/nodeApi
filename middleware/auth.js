1e8ec3f4eb1953bd38cda6e89acdee108f67a675
Hardik <prajapatihardik2910@gmail.com>
2023-06-04 10:00:19 +0530

    little Change

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,6 +12,6 @@ export const isAuthenticated = async (req, res, next) => {
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  req.user = await User.findById(decoded);
+  req.user = await User.findById(decoded._id);
   next();
 };
